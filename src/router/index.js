2529d3556bf28930a9e498db6e5f96af6866929f
Leonardo Adamoli <leo.adamoli@gmail.com>
2025-07-03 23:28:15 -0300

    fix: Delaying the scroll due to CSS transitions

--- src/router/index.js
+++ src/router/index.js
@@ -56,11 +56,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior() {
-    return {
-      top: 0,
-      behavior: "smooth",
-    };
+  async scrollBehavior() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          top: 0,
+          behavior: "smooth",
+        });
+      }, 500);
+    });
   },
 });
 
